68e7428571f076237591a4aff0b8da1a94c8ea53
בר אביזמר <bar.avizemer@gmail.com>
2026-03-15 05:54:12 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { pagesConfig } from './pages.config'
 import MyParking from './pages/MyParking'
 import Bookings from './pages/Bookings'
+import AdminDashboard from './pages/AdminDashboard'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
